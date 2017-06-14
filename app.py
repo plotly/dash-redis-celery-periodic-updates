335a4d47061aa23bdc39c576d4e338a20f057da9
chriddyp <chris@plot.ly>
2017-06-14 14:12:51 -0400

    app

---
+++ app.py
@@ -0,0 +1,40 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+from pandas_datareader import data as web
+from datetime import datetime as dt
+import flask
+
+server = flask.Flask('app')
+
+app = dash.Dash('app', server=server)
+
+app.layout = html.Div([
+    html.H1('Stock Tickers'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
+    dcc.Graph(id='my-graph')
+])
+
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    df = web.DataReader(
+        selected_dropdown_value, data_source='google',
+        start=dt(2017, 1, 1), end=dt.now())
+    return {
+        'data': [{
+            'x': df.index,
+            'y': df.Close
+        }]
+    }
+
+if __name__ == '__main__':
+    app.run_server()
