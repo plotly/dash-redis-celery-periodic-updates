90d497a310340e896553fa9e95a6f9efa4f54480
Jody McIntyre <scjody@modernduck.com>
2018-03-30 17:12:43 -0400

    Replace Pandas queries with Redis INFO

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-from pandas_datareader import data as web
 from datetime import datetime as dt
 import flask
 import redis
@@ -20,46 +19,15 @@ if 'DYNO' in os.environ:
             os.environ['DASH_APP_NAME']
         )
 
+r = redis.StrictRedis.from_url(os.environ['REDIS_URL'])
+
 app.scripts.config.serve_locally = False
 dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
 
 app.layout = html.Div([
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Coke', 'value': 'COKE'},
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'}
-        ],
-        value='COKE'
-    ),
-    dcc.Graph(id='my-graph')
+    html.H1('Redis INFO'),
+    html.Div(children=html.Pre(str(r.info())))
 ], className="container")
-
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
-    df = web.DataReader(
-        selected_dropdown_value, data_source='google',
-        start=dt(2017, 1, 1), end=dt.now())
-    return {
-        'data': [{
-            'x': df.index,
-            'y': df.Close,
-            'line': {
-                'width': 3,
-                'shape': 'spline'
-            }
-        }],
-        'layout': {
-            'margin': {
-                'l': 30,
-                'r': 20,
-                'b': 30,
-                't': 20
-            }
-        }
-    }
 
 app.css.append_css({
     'external_url': (
